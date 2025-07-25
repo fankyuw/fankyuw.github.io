2f16410f46608c02429291b19be9ebe05e7606a0
fankyu <fankydoesart@gmail.com>
2025-07-25 19:33:10 -0400

    omg please stop

--- random-music/music.js
+++ random-music/music.js
@@ -34,7 +34,7 @@ musicFolders.forEach(folder =>{
             });
             musicPaths.forEach(element => { 
                 const currentDiv = document.getElementById(folder).appendChild(divNode.cloneNode())
-                var newText = element.replaceAll("%"," ")
+                var newText = element.replaceAll("%20"," ")
                 currentDiv.innerHTML = newText
                 currentDiv.appendChild(document.createElement("br"))
                 const currentAudioNode = currentDiv.appendChild(audioNode.cloneNode())
